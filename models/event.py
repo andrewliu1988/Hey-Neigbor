7178201bba93c29a44bbc3644b65bb9f23e3363f
Andrew Liu <liu.andrew1988@gmail.com>
2021-04-12 16:32:03 -0400

    {event.py} create model for event

--- models/event.py
+++ models/event.py
@@ -1,33 +1,35 @@
-from datatime import datetime
+from datetime import datetime
 from models.db import db
 
 
-class Business(db.Model):
-    __tablename__ = 'businesses'
+class Event(db.Model):
+    __tablename__ = 'events'
 
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255), nullable=False)
     address = db.Column(db.String(255), nullable=False)
-    description = db.Column(db.string(255), nullable=False)
+    description = db.Column(db.String(255), nullable=False)
     date = db.Column(db.String(50), nullable=False)
     zipcode = db.Column(db.Integer(20), nullable=False)
     website = db.Column(db.String(255), nullable=False)
     longitude = db.Column(db.String(50), nullable=False)
     langitude = db.Column(db.String(50), nullable=False)
+    attendees = db.Column(db.Ineger(10), nullable=False)
     created_at = db.Column(
         db.DateTime, default=datetime.utcnow, nullable=False)
     updated_at = db.Column(db.DateTime, default=datetime.utcnow,
                            nullable=False, onupdate=datetime.now())
 
-    def __init__(self, name, address, description, date, zipcode, website, longitude, langitude):
+    def __init__(self, name, address, description, date, zipcode, website, longitude, langitude, attendees):
         self.name = name
-        self.address = address
+        self.adddress = address
         self.description = description
         self.date = date
         self.zipcode = zipcode
         self.website = website
         self.longitude = longitude
         self.langitude = langitude
+        self.attendees = attendees
 
     def json(self):
         return {"id": self.id,
@@ -38,26 +40,28 @@ class Business(db.Model):
                 "website": self.website,
                 "longitude": self.longitude,
                 "langitude": self.langitude,
+                "attendees": self.attendees,
                 "created_at": str(self.created_at),
                 "updated_at": str(self.updated_at)}
 
-    def create(self):
+      def create(self):
         db.session.add(self)
         db.session.commit()
-        return self
+        return self          
 
-    @classmethod
-    def find_all(cls):
-        businesses = Business.query.all()
-        return [business.json() for business in businesses]
+      @classmethod
+      def find_all(cls):
+        events = Event.query.all()
+        return [event.json() for event in events]
 
-    @classmethod
-    def find_by_id(cls, id):
-        return Business.query.filter_by(id=id), first()
+      @classmethod
+      def find_by_id(cls, id):
+        return Event.query.filter_by(id=id), first()
 
-    @classmethod
-    def delete(cls, id):
-        business = Business.find_by_id(id)
-        db.session.delete(post)
+
+      @classmethod
+      def delete(cls, id): 
+        event = Event.find_by_id(id)
+        db.session.delete(event)
         db.session.commit()
-        return business.json()
+        return event.json()
