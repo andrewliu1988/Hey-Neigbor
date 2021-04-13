ed0306ac75b16acc656968e984cdea239007982b
Andrew Liu <liu.andrew1988@gmail.com>
2021-04-13 12:34:22 -0400

    build action for business

---
+++ client/src/store/actions/BusinessAction.js
@@ -0,0 +1,50 @@
+import {
+  __GetALlBusiness,
+  __GetBusinessDetails,
+  __GetBusinessByZipcode
+} from '../../services/BusinessService'
+
+import {
+  GET_ALL_BUSINESS,
+  GET_BUSINESSES_BY_ZIPCODE,
+  GET_BUSINESS_DETIALS
+} from '../types'
+
+export const GetAllBusiness = () => async (dispatch) => {
+  try {
+    const businesses = await __GetALlBusiness()
+    console.log(businesses)
+    dispatch({
+      type: GET_ALL_BUSINESS,
+      payload: businesses
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetBusinessDetails = (id) => async (dispatch) => {
+  try {
+    const business = await __GetBusinessDetails(id)
+    console.log(business)
+    dispatch({
+      type: GET_BUSINESS_DETIALS,
+      payload: business
+    })
+  } catch (error) {
+    throw error
+  }
+}
+
+export const GetBusinessByZipcode = (zipcode) => async (dispatch) => {
+  try {
+    const zipC = await __GetBusinessByZipcode(zipcode)
+    console.log(zipC)
+    dispatch({
+      type: GetBusinessByZipcode,
+      payload: zipC
+    })
+  } catch (error) {
+    throw error
+  }
+}
