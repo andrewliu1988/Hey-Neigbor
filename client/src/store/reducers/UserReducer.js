15f59bccfbf07e31122abdc3ef3a1cf0db7b91b9
Andrew Liu <liu.andrew1988@gmail.com>
2021-04-14 00:36:10 -0400

    add types and delete funciont for business and events

--- client/src/store/reducers/UserReducer.js
+++ client/src/store/reducers/UserReducer.js
@@ -1,4 +1,8 @@
-import { USER_BUSINESSES_AND_EVENTS } from '../types'
+import {
+  USER_BUSINESSES_AND_EVENTS,
+  DELETE_BUSINESS,
+  DELETE_EVENT
+} from '../types'
 
 const iState = {
   userBAndE: []
@@ -8,6 +12,10 @@ const UserReducer = (state = iState, action) => {
   switch (action.type) {
     case USER_BUSINESSES_AND_EVENTS:
       return { ...state, userBAndE: action.payload }
+    case DELETE_BUSINESS:
+      return { ...state }
+    case DELETE_EVENT:
+      return { ...state }
     default:
       return { ...state }
   }
